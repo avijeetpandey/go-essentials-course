493e0d81c4f26daa3fbb9dffa99828b6281ae40c
avijeet <avijeetpandey25@gmail.com>
2024-06-23 14:08:43 +0530

    Add: cli instructions

--- investment_calculator.go
+++ investment_calculator.go
@@ -6,10 +6,22 @@ import (
 )
 
 func main() {
-	const inflationRate = 2.5
-	var investmentAmount float64 = 1000
-	years := 5.5
-	expectedReturnRate := 10.0
+	var inflationRate float64
+	var investmentAmount float64
+	var years float64
+	var expectedReturnRate float64
+
+	fmt.Print("Please enter investment amount: ")
+	fmt.Scan(&investmentAmount)
+
+	fmt.Print("Please enter the number of years: ")
+	fmt.Scan(&years)
+
+	fmt.Print("Please enter expected return rate: ")
+	fmt.Scan(&expectedReturnRate)
+
+	fmt.Print("Please enter expected inflation: ")
+	fmt.Scan(&inflationRate)
 
 	var futureValue = investmentAmount * math.Pow(1+expectedReturnRate/100, years)
 
