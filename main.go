4180a76c711f7a52036ce1a759a24056d1ac26d0
avijeet <avijeetpandey25@gmail.com>
2024-06-23 15:52:27 +0530

    Add: user instantiation

--- main.go
+++ main.go
@@ -3,7 +3,15 @@ package main
 import (
 	"fmt"
 	"go-essentials/fileops"
+	"time"
 )
+
+type user struct {
+	firstName string
+	lastName  string
+	birthDate string
+	createdAt time.Time
+}
 
 func main() {
 	balance, err := fileops.ReadFloatFromFile("balance.txt")
@@ -15,4 +23,15 @@ func main() {
 	fmt.Println(balance)
 
 	fileops.WriteFloatToFile(100000)
+
+	// instantiating user
+
+	appUser := user{
+		firstName: "Avijeet",
+		lastName:  "pandey",
+		birthDate: "11/08/1998",
+		createdAt: time.Now(),
+	}
+
+	fmt.Println(appUser)
 }
